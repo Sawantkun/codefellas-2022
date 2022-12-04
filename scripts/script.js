07b7f4b50fe3be2cc1c9044f827b6e1dacfc800a
Sawantkun <kumar.sawant37@gmail.com>
2022-12-04 21:44:25 +0530

    changed modal

--- scripts/script.js
+++ scripts/script.js
@@ -23,17 +23,40 @@ var btn = document.getElementById("myBtn");
 var btn2 = document.querySelector(".gotit");
 var span = document.getElementsByClassName("close")[0];
 
+
+function disableScrolling(){
+  var x=window.scrollX;
+  var y=window.scrollY;
+  window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector("nav").style.top = "0px";
+    } else {
+      document.querySelector("nav").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+  };}
+
 btn.onclick = function() {
+  disableScrolling();
+  document.querySelector("nav").style.top = "-80px";
   modal.style.display = "block";
 }
 btn2.onclick = function() {
   modal.style.display = "none";
+  enableScrolling();
 }
 span.onclick = function() {
   modal.style.display = "none";
+  enableScrolling();
 }
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    enableScrolling();
   }
 }
